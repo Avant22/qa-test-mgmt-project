a1be27eabd7126f63f13d89128af7f1a80b3be74
ThePurplePen <ThePurplePen@ThePurplePen.homenet.telecomitalia.it>
2025-10-03 11:12:40 +0200

    Fix Selenium wait timing + Postman env var case

--- selenium/tests/test_checkout.py
+++ selenium/tests/test_checkout.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
 
 @pytest.fixture
@@ -17,13 +19,15 @@ def browser():
 
 @pytest.mark.regression
 def test_checkout_flow(browser):
+    wait = WebDriverWait(browser, 10)
+
     browser.get("https://www.saucedemo.com/")
     browser.find_element(By.ID, "user-name").send_keys("standard_user")
     browser.find_element(By.ID, "password").send_keys("secret_sauce")
     browser.find_element(By.ID, "login-button").click()
 
-    browser.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
+    wait.until(EC.element_to_be_clickable((By.ID, "add-to-cart-sauce-labs-backpack"))).click()
     browser.find_element(By.CLASS_NAME, "shopping_cart_link").click()
-    browser.find_element(By.ID, "checkout").click()
+    wait.until(EC.element_to_be_clickable((By.ID, "checkout"))).click()
 
     assert "Checkout" in browser.page_source
